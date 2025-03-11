e0b7bf28cab8c7886c0b1d90514cca9aed2e59ce
hichemmezemate <hichem.mezemate@gmail.com>
2025-03-11 14:05:59 +0100

    setting JWT + login and interface page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { LoginService } from '../services/login.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule,MatIconModule, FormsModule, MatFormFieldModule, MatInputModule, FormsModule, MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  
+  constructor(private loginService: LoginService, private router: Router) {}
+  
+  isLogged: boolean = false
+  error: string = ""
+
+
+  ngOnInit() {
+    const token = localStorage.getItem('access_token');
+    console.log(token)
+
+    if (token) {
+      this.router.navigate(['/interface']);
+    }
+  }
+
+  login() {
+    // this.loginService.login(this.username, this.password).subscribe(
+    //   response => {
+    //     this.isLogged = true
+    //     localStorage.setItem('access_token',response.access)
+    //     localStorage.setItem('refresh_token',response.refresh)
+    //     this.router.navigate(['/interface'])
+    //   },
+    //   error => {
+    //     console.error('Login failed:', error);
+    //     this.error = "Authentification echouée"
+    //   }
+    // );
+    this.loginService.login(this.username, this.password).subscribe({
+      next: (data) => {
+        this.isLogged = this.loginService.isAuthenticated()
+        localStorage.setItem('access_token',data.access)
+        localStorage.setItem('refresh_token',data.refresh)
+        this.router.navigate(['/interface'])
+      },
+      error: (error) => {
+        console.error('Login failed:', error.message);
+        this.error = "Identifiants erronés"
+      }
+    });
+  }
+
+}
